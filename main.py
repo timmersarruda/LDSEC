81ea816f8881736321881eb63d702fe8dbd2bf3c
Timmers Arruda <31082154+timmersarruda@users.noreply.github.com>
2022-03-30 01:32:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,43 +2,21 @@ import Ldsec
 
 lista = Ldsec.Ldsec()
 
-if not lista.estahVazia():
-    lista.removerInicio()
 
-lista.inserirInicio('C')
-lista.inserirInicio('B')
-lista.inserirInicio('A')
+lista.inserirInicio('3')
+lista.inserirInicio('2')
+lista.inserirInicio('1')
 lista.imprimir()
-lista.inserirFim('D')
-lista.inserirFim('E')
+lista.inserirFim('4')
+lista.inserirFim('5')
 
 print('Primeiro elemento da lista: ', lista.getFirstElement())
 print('Ultimo elemento da lista: ', lista.getLastElement())
 print('Quantidade de elementos da lista: ', lista.getQuant())
-print('Removendo o primeiro elemento: ')
-print('Removendo o ultimo elemento: ')
+print('Removendo o primeiro elemento: ', lista.removerInicio())
 lista.imprimir()
-
-lista.removerInicio()
+print('Removendo o ultimo elemento: ', lista.removerFim())
 lista.imprimir()
-lista.removerFim()
+print("Contando todos os pares: ", lista.contarPares())
+print("Consultando elemento 3 na lista: ", lista.consultarElemento(3))
 lista.imprimir()
-
-
-
-
-
-'''
-1) Sobre Listas Dinâmicas Simplesmente Encadeadas, implementar os seguintes métodos: 
-inserir elemento em uma determinada posição na lista - inserirPosicao(valor, posicao)
-remover elemento em uma determinada posição na lista - removerPosicao(posicao)
-remover todas as ocorrências de um determinado elemento na lista - removerTodas(valor)
-inserir um elemento após determinado elemento - inserirApos(valor1, valor2)  PS: valor1 = 
-
-elemento a ser pesquisado na lista e valor2 = o elemento a ser inserido após valor1 
-somar todos os elementos da lista - somarElementos() - PS: considerar que os elementos da lista sejam números
-2) Sobre Listas Dinâmicas Simplesmente Encadeadas Circulares, implementar os seguintes métodos: 
-trocar um elemento em uma determinada posição na lista por outro elemento - trocar(posicao, novovalor)
-consultar o elemento anterior a um determinado elemento na lista - consultarAnterior(valor)
-contar quantidade de elementos pares na lista - contarPares()
-'''
