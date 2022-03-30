bf74e7aa3fec7ff7852949db85842608397162a3
Timmers Arruda <31082154+timmersarruda@users.noreply.github.com>
2022-03-30 01:31:29 -0300

    Update Ldsec.py

--- Ldsec.py
+++ Ldsec.py
@@ -1,74 +1,93 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 18 13:23:26 2022
-
-@author: PC
-"""
-
 class No:
     def __init__(self, info, proximo):
         self.info = info
         self.prox = proximo
-        
+
 
 class Ldsec:
     def __init__(self):
         self.prim = self.ult = None
         self.quant = 0
 
-    def estahVazia(self):
+    def estaVazia(self):
         return self.quant == 0
-    
+
     def getQuant(self):
-        return self.quant 
-    
-    def getFistElement(self):
-        if not self.estahVazia():
+        return self.quant
+
+    def getFirstElement(self):
+        if not self.estaVazia():
             return self.prim.info
-        
+
     def getLastElement(self):
-        if not self.estahVazia():
+        if not self.estaVazia():
             return self.ult.info
-        
+
     def inserirInicio(self, valor):
         if self.quant == 0:
             self.prim = self.ult = No(valor, None)
             self.prim.prox = self.prim
         else:
             self.prim = No(valor, self.prim)
-            self.ult.prox = self.prim 
+            self.ult.prox = self.prim
         self.quant += 1
-            
+
     def inserirFim(self, valor):
-        if self.quant == 0 :
+        if self.quant == 0:
             self.prim = self.ult = No(valor, None)
+            self.prim.prox = self.prim
         else:
             self.ult.prox = self.ult = No(valor, self.prim)
-        self.quant += 1    
-        
+        self.quant += 1
+
     def removerInicio(self):
         if self.quant == 1:
             self.prim = self.ult = None
         else:
-            self.prim = self.ult.prox
-            self.prim.prox = self.prim
+            self.prim = self.prim.prox
+            self.ult.prox = self.prim
         self.quant -= 1
-        
+
     def removerFim(self):
         if self.quant == 1:
             self.prim = self.ult = None
         else:
-            aux = self.prim 
+            aux = self.prim
             while aux.prox != self.ult:
-                aux = aux.prox 
-                aux.prox = self.prim 
+                aux = aux.prox
             aux.prox = self.prim
-            self.ult = aux 
-        self.quant -= 1 
-                   
+            self.ult = aux
+        self.quant -= 1
+
     def imprimir(self):
         aux = self.prim
-        while aux.prox != self.ult:
+        i = 0
+        while i < self.quant:
             print(aux.info, end=' ')
             aux = aux.prox
-            print() 
+            i += 1
+        print()
+    
+    def contarPares(self):
+        aux  = self.prim
+        par = 0;
+        while self.ult.prox != self.prim:
+            aux.info = aux 
+            aux = self.ult
+            if self.info % 2 == 0:
+                par += 1;
+        return par
+
+    def consultarElemento(self, valor):
+        aux = self.prim
+        if self.quant == 1:
+            aux.prox = self.ult
+            if self.quant == valor:
+                aux = aux.prox = self.prim
+                print("Elemento existente ", valor)
+            else:
+                while aux.prox != self.prim:
+                  aux = aux.prox
+                  aux.prox.info = self.ult
+            print(valor)
+            
